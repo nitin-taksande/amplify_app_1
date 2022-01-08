2539967249649cf3616af6e3c33ca816deee2081
Divya Mallela <divyamallela@Divyas-MacBook-Pro.local>
2022-01-08 14:37:28 -0500

    Logo change

--- src/App.js
+++ src/App.js
@@ -11,14 +11,6 @@ function App() {
         <p>
           Hello to our app!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
